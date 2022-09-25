6dce417014e2cc1c98d2f1dd3d566151a3267833
Saka Sheriff Alade <sheriffdeenalade@gmail.com>
2022-09-25 05:14:02 +0100

    Adding the logic for the form fields.

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,40 +1,39 @@
 import React, { Component, useState, createRef } from 'react';
 import {ScrollView, View, Text, TextInput, StyleSheet, Button, 
   Image, Keyboard, TouchableOpacity, KeyboardAvoidingView} from 'react-native';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+// import { Formik, Form, Field } from 'formik';
+// import * as Yup from 'yup';
 import AppText from '../components/AppText';
-
-// const validationSchema = Yup.object.shape({
-//   email:Yup.string().email('Please enter valid email address').required('Email is required').label('E-mail'),
-//   password: Yup.string().matches(/\w*[a-z]\w*/,'Password must have small letters')
-//   .matches(/\w*[A-Z]\w*/,'Password must have small letters')
-//   .matches(/\d/, 'Passowrd must have m=number')
-//   .min(8, ({min}) => `Password must be at least ${min} characters`)
-//   .required('Password is required').label('Password')
-// });
 
 
 
 export default class HomeScreen extends Component {
-    
+  constructor(props){
+    super(props);
+    this.state={email: '', password: '', telephone: '', eventtype: ''}
+  }
+  
+  InsertRecord = () =>{
+    var email = this.state.email;
+    var password = this.state.password;
+    var telephone = this.state.telephone;
+    var eventtype = this.state.eventtype;
+
+    if(email.length == 0 || password.length == 0 || telephone.length == 0 || eventtype.length == 0)
+    {
+      alert("Required field is missing");
+    }else{
+      alert("Success - Fetching the API code here.");
+    }
+  }
   render() {  
     return (
       
-      <Formik 
-      // validationSchema = {validationSchema}
-           initialValues={{email: '', password: '', telephone: '', eventtype: '' }}
-           onSubmit={values => console.log(values)}
-      >
-        {({
-           handleChange, handleBlur, handleSubmit, values, errors, touched,
-       }) => (
-        <>
+      
       <ScrollView style={styles.ViewStyle}>
         <KeyboardAvoidingView>
         <Button
             title="Already Registered Login Here"
-            // onPress={() => this.props.navigation.navigate('EventScreen')}
             onPress={() => this.props.navigation.navigate('LoginScreen')}
             style={{marginBottom: 10, padding: 20,}}
         />
@@ -50,59 +49,48 @@ export default class HomeScreen extends Component {
             placeholderTextColor={"#FF0000"}
             keyboardType="email-address"
             style={styles.txtStyle}
+            onChange={email=>this.setState({email})}
         />
-        <Text style={{color: 'red'}}>{errors.email}</Text>
         <TextInput 
             placeholder={"Password"}
             name = "password"
             placeholderTextColor={"#FF0000"}
             secureTextEntry={true}
             style={styles.txtStyle}
+            onChange={password=>this.setState({password})}
         />
-        <Text style={{color: 'red'}}>{errors.password}</Text>
+      
         <TextInput 
             placeholder={"Telephone"}
             name = "telephone"
             placeholderTextColor={"#FF0000"}
-            value = {values.telephone}
             style={styles.txtStyle}
+            onChange={telephone=>this.setState({telephone})}
         />
-        <Text style={{color: 'red'}}>{errors.telephone}</Text>
+      
         <TextInput 
             placeholder={"Event Type"}
             name = "eventtype"
             placeholderTextColor={"#FF0000"}
             style={styles.txtStyle}
+            onChange={eventtype=>this.setState({eventtype})}
         />
-        <Text style={{color: 'red'}}>{errors.eventtype}</Text>
-
+        
         <Button style={styles.registerButton}
-        title={"Save Event"}  onPress ={handleSubmit}/>
+        title={"Save Event"}  onPress ={this.InsertRecord}/>
         
         <View>
         <Text>Appmart Integrated Limited Event Registration</Text>
         </View>
 
         </KeyboardAvoidingView>
-      </ScrollView>
-          </>
-       )}
-      </Formik>
-      
+      </ScrollView>   
       
     )
   }
 };
 
 
-// const validationSchema = Yup.object.shape({
-//   email:Yup.string().email('Please enter valid email address').required('Email is required').label('E-mail'),
-//   password: Yup.string().matches(/\w*[a-z]\w*/,'Password must have small letters')
-//   .matches(/\w*[A-Z]\w*/,'Password must have small letters')
-//   .matches(/\d/, 'Passowrd must have m=number')
-//   .min(8, ({min}) => `Password must be at least ${min} characters` )
-//   .required('Password is required').label('Password')
-// });
 
 const styles = StyleSheet.create({
     ViewStyle:{
